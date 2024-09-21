6bc4e11f362074e0f80f237d7cb997967d2998b3
Robeeen <robeeen@gmail.com>
2024-09-21 23:15:00 +0600

    Adding feature of Toggle on/off for Search on admin

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '13aa28a4f23480f1390c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '172fccaf6d42fee9ab65');
