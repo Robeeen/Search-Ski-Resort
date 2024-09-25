cc082d6572492a62e5ef04b042c3be8f57980f6e
Robeeen <robeeen@gmail.com>
2024-09-25 23:19:51 +0600

    added cache policy set_transient and get_transient

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9110dd5b6b1e068b9284');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0de739e750792b585b52');
